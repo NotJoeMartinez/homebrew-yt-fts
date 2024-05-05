85b8623380cb1ab56dcf773b033195888f285ee6
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2024-05-05 01:23:58 -0500

    v0.1.47

--- Formula/yt-fts.rb
+++ Formula/yt-fts.rb
@@ -523,7 +523,7 @@ class YtFts < Formula
     # Test basic functionality
     system bin/"yt-fts", "--version"
 
-    assert_match "yt_fts version: 0.1.44", shell_output("#{bin}/yt-fts --version")
+    assert_match "yt_fts version: 0.1.47", shell_output("#{bin}/yt-fts --version")
 
   end
 end
